03e1f07955dae6b517ef6cfbff003c55a3374df7
BrihlyaevRA <ruslan5327@rambler.ru>
2013-11-29 11:04:16 +0400

    Справочник "Страны".

--- middleoffice/src/main/java/ru/prbb/middleoffice/repo/dictionary/CountriesDaoImpl.java
+++ middleoffice/src/main/java/ru/prbb/middleoffice/repo/dictionary/CountriesDaoImpl.java
@@ -3,15 +3,16 @@
  */
 package ru.prbb.middleoffice.repo.dictionary;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import ru.prbb.Utils;
 import ru.prbb.middleoffice.domain.CountryItem;
 import ru.prbb.middleoffice.domain.SimpleItem;
 
@@ -28,33 +29,28 @@ public class CountriesDaoImpl implements CountriesDao
 	@Autowired
 	private EntityManager em;
 
-	/**
-	 * @return
-	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<CountryItem> findAll() {
 		// {call dbo.mo_WebGet_Countries_sp}
-		final List<CountryItem> list = new ArrayList<CountryItem>();
-		for (int i = 0; i < 10; i++) {
-			final CountryItem item = new CountryItem();
-			item.setId(i + 1L);
-			item.setName("name" + (i + 1));
-			list.add(item);
-		}
-		return list;
+		String sql = "execute dbo.mo_WebGet_Countries_sp";
+		Query q = em.createNativeQuery(sql, CountryItem.class);
+		return q.getResultList();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<SimpleItem> findCombo(String query) {
-		// select id, name from dbo.mo_WebGet_ajaxCountries_v
-		// " where lower(name) like ?"
-		final List<SimpleItem> list = new ArrayList<SimpleItem>();
-		for (int i = 0; i < 10; i++) {
-			final SimpleItem item = new SimpleItem();
-			item.setId(i + 1L);
-			item.setName("name" + (i + 1));
-			list.add(item);
+		String sql = "select id, name from dbo.mo_WebGet_ajaxCountries_v";
+		Query q;
+		if (Utils.isEmpty(query)) {
+			q = em.createNativeQuery(sql, SimpleItem.class);
+		} else {
+			sql += " where lower(name) like ?";
+			q = em.createNativeQuery(sql, SimpleItem.class)
+					.setParameter(1, query.toLowerCase() + '%');
 		}
-		return list;
+		return q.getResultList();
 	}
+
 }
