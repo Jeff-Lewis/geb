7522b9e6c78b5c9593fbcda0449381c58f0021b1
Ruslan <ruslan5327@rambler.ru>
2014-06-23 12:08:10 +0400

    Добавлен обработчик исключений.

---
+++ analytics/src/main/java/ru/prbb/analytics/rest/BaseController.java
@@ -0,0 +1,35 @@
+package ru.prbb.analytics.rest;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.PersistenceException;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public abstract class BaseController {
+
+	@ExceptionHandler
+	@ResponseBody
+	private Map<String, Object> handleException(Exception ex) {
+		String message = ex.getMessage();
+
+		if (ex instanceof PersistenceException) {
+			try {
+				Throwable cause = ex.getCause();
+				SQLException sqlException = (SQLException) cause.getCause();
+				message = sqlException.getMessage();
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+
+		Map<String, Object> res = new HashMap<String, Object>(4);
+		res.put("success", Boolean.FALSE);
+		res.put("error", message);
+		return res;
+	}
+
+}
