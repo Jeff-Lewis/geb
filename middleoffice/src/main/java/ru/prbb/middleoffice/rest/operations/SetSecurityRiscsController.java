60060d2e7f4d5665864473df4ecbed2e648f8bf7
Ruslan <ruslan5327@rambler.ru>
2014-09-22 11:34:04 +0400

    Отображение уже заданных параметров риска

--- middleoffice/src/main/java/ru/prbb/middleoffice/rest/operations/SetSecurityRiscsController.java
+++ middleoffice/src/main/java/ru/prbb/middleoffice/rest/operations/SetSecurityRiscsController.java
@@ -11,8 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import ru.prbb.Utils;
 import ru.prbb.middleoffice.domain.Result;
+import ru.prbb.middleoffice.domain.ResultData;
+import ru.prbb.middleoffice.domain.SecurityRiscsItem;
+import ru.prbb.middleoffice.domain.SimpleItem;
 import ru.prbb.middleoffice.domain.ViewPortfolioTransferItem;
+import ru.prbb.middleoffice.repo.EquitiesDao;
+import ru.prbb.middleoffice.repo.dictionary.ClientsDao;
+import ru.prbb.middleoffice.repo.dictionary.FundsDao;
 import ru.prbb.middleoffice.repo.operations.SetSecurityRiscsDao;
+import ru.prbb.middleoffice.repo.portfolio.SecurityRiscsDao;
 import ru.prbb.middleoffice.repo.portfolio.ViewPortfolioDao;
 import ru.prbb.middleoffice.rest.BaseController;
 
@@ -31,6 +38,15 @@ public class SetSecurityRiscsController
 	private SetSecurityRiscsDao dao;
 	@Autowired
 	private ViewPortfolioDao daoPortfolio;
+
+	@Autowired
+	private SecurityRiscsDao daoSecurityRiscs;
+	@Autowired
+	private EquitiesDao daoEquities;
+	@Autowired
+	private ClientsDao daoClients;
+	@Autowired
+	private FundsDao daoFunds;
 
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
@@ -54,5 +70,29 @@ public class SetSecurityRiscsController
 	{
 		log.info("POST SetSecurityRiscs/Portfolio: date={}", date);
 		return daoPortfolio.executeSelect(Utils.parseDate(date));
+	}
+
+	@RequestMapping(value = "/UpdateFields", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public Result postUpdate(
+			@RequestParam String security,
+			@RequestParam String client,
+			@RequestParam String fund,
+			@RequestParam Integer batch,
+			@RequestParam String date)
+	{
+//		log.info("POST SetSecurityRiscs/Update: security={}, fund={}, batch={}, client={}, date={}",
+//				Utils.toArray(security, fund, batch, client, date));
+
+		Long _security = getId(daoEquities.findCombo(security));
+		Long _fund = getId(daoFunds.findCombo(fund));
+		Long _client = getId(daoClients.findCombo(client));
+		List<SecurityRiscsItem> res = daoSecurityRiscs.findAll(_security, _fund, batch, _client, Utils.parseDate(date));
+
+		return res.isEmpty() ? Result.FAIL : new ResultData(res.get(0));
+	}
+
+	private Long getId(List<SimpleItem> list) {
+		return list.isEmpty() ? null : list.get(0).getId();
 	}
 }
