5382e98967eb92427275ce5b5a5aec5ebaab0e63
ruslan <ruslan@MSI-CX70-2OD-U1404>
2015-03-17 19:36:03 +0300

    Работа с Агентом и Активным Агентом.

---
+++ jobber/src/main/java/ru/prbb/jobber/repo/BloombergServicesAgentImpl.java
@@ -0,0 +1,264 @@
+package ru.prbb.jobber.repo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import ru.prbb.jobber.domain.SecForJobRequest;
+import ru.prbb.jobber.domain.SecurityItem;
+import ru.prbb.jobber.domain.SubscriptionItem;
+
+/**
+ * @author RBr
+ * 
+ */
+@Service
+public class BloombergServicesAgentImpl implements BloombergServicesJ {
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	/**
+	 * @param name
+	 * @param items
+	 * @return
+	 */
+	private Collection<NameValuePair> createList(String name, String[] items) {
+		List<NameValuePair> list = new ArrayList<>(items.length);
+		for (String s : items) {
+			list.add(new BasicNameValuePair(name, s));
+		}
+		return list;
+	}
+
+	private String executeHttpRequest(String path, List<NameValuePair> nvps, String name) {
+		nvps.add(new BasicNameValuePair("name", name));
+		try {
+			URI uri = new URIBuilder()
+					.setScheme("http")
+					.setHost("172.16.15.117")
+					// .setHost(InetAddress.getLocalHost().getHostAddress())
+					.setPort(48080)
+					.setPath(path)
+					.build();
+
+			HttpPost httpPost = new HttpPost(uri);
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			try {
+				log.debug("Executing request " + httpPost.getRequestLine());
+				String responseBody = httpclient.execute(httpPost, new ResponseHandler<String>() {
+
+					public String handleResponse(final HttpResponse response)
+							throws ClientProtocolException, IOException {
+						int status = response.getStatusLine().getStatusCode();
+						if (status >= HttpStatus.SC_OK && status < HttpStatus.SC_MULTIPLE_CHOICES) {
+							HttpEntity entity = response.getEntity();
+							return entity != null ? EntityUtils.toString(entity) : null;
+						} else {
+							throw new ClientProtocolException("Unexpected response status: " + status);
+						}
+					}
+
+				});
+				log.debug("Response " + responseBody);
+				if (responseBody.contains("stackTrace")) {
+					@SuppressWarnings("unchecked")
+					Map<String, Object> res =
+							(Map<String, Object>) deserialize(responseBody);
+					Object message = res.get("message");
+					throw new Exception((message != null) ? message.toString() : res.toString());
+				}
+				return responseBody;
+			} finally {
+				httpclient.close();
+			}
+		} catch (Exception e) {
+			log.error("executeHttpRequest", e);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private final ObjectMapper m = new ObjectMapper();
+
+	private Object deserialize(String content) {
+		try {
+			log.info(content);
+			return m.readValue(content, Object.class);
+		} catch (Exception e) {
+			log.error("deserialize", e);
+		}
+		throw new RuntimeException("deserialize");
+	}
+
+	/**
+	 * BDS запрос
+	 * 
+	 * @param name
+	 * @param securities
+	 * @param fields
+	 * @return [Peers, EARN_ANN_DT_TIME_HIST_WITH_EPS, ERN_ANN_DT_AND_PER,
+	 *         PeerTicker, BEST_ANALYST_RECS_BULK]
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> executeBdsRequest(String name, String[] securities, String[] fields) {
+		List<NameValuePair> nvps = new ArrayList<>();
+		nvps.addAll(createList("securities", securities));
+		nvps.addAll(createList("fields", fields));
+
+		String response = executeHttpRequest("/BdsRequest", nvps, name);
+		return (Map<String, Object>) deserialize(response);
+	}
+
+	/**
+	 * Выполнить запрос //blp/refdata, ReferenceDataRequest<br>
+	 * Добавить несколько компаний
+	 * 
+	 * @param name
+	 *            Название запроса
+	 * @param securities
+	 *            Коды
+	 * @param fields
+	 *            Поля
+	 * @return security -> {field, value}
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, Map<String, String>> executeReferenceDataRequest(String name,
+			String[] securities, String[] fields) {
+		List<NameValuePair> nvps = new ArrayList<>();
+		nvps.addAll(createList("securities", securities));
+		nvps.addAll(createList("fields", fields));
+
+		String response = executeHttpRequest("/ReferenceData", nvps, name);
+		return (Map<String, Map<String, String>>) deserialize(response);
+	}
+
+	/**
+	 * Загрузка котировок
+	 * 
+	 * @param name
+	 *            Название запроса
+	 * @param startDate
+	 * @param endDate
+	 * @param securities
+	 *            Коды
+	 * @param fields
+	 *            Поля
+	 * @return security -> {date -> { field, value } }
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, Map<String, Map<String, String>>> executeHistoricalDataRequest(String name,
+			Date startDate, Date endDate, String[] securities, String[] fields) {
+		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		List<NameValuePair> nvps = new ArrayList<>();
+		nvps.add(new BasicNameValuePair("dateStart", sdf.format(startDate)));
+		nvps.add(new BasicNameValuePair("dateEnd", sdf.format(endDate)));
+		nvps.addAll(createList("securities", securities));
+		nvps.addAll(createList("fields", fields));
+
+		String response = executeHttpRequest("/HistoricalDataRequest", nvps, name);
+		return (Map<String, Map<String, Map<String, String>>>) deserialize(response);
+	}
+
+	/**
+	 * Загрузка котировок
+	 * 
+	 * @param name
+	 *            Название запроса
+	 * @param startDate
+	 * @param endDate
+	 * @param securities
+	 *            Коды
+	 * @param fields
+	 *            Поля
+	 * @return security -> {date -> { field, value } }
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, Map<String, Map<String, String>>> executeHistoricalDataRequest(String name,
+			Date startDate, Date endDate, String[] securities, String[] fields, String[] currencies) {
+		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		List<NameValuePair> nvps = new ArrayList<>();
+		nvps.add(new BasicNameValuePair("dateStart", sdf.format(startDate)));
+		nvps.add(new BasicNameValuePair("dateEnd", sdf.format(endDate)));
+		nvps.addAll(createList("securities", securities));
+		nvps.addAll(createList("fields", fields));
+		nvps.addAll(createList("currencies", currencies));
+
+		String response = executeHttpRequest("/HistoricalDataRequest", nvps, name);
+		return (Map<String, Map<String, Map<String, String>>>) deserialize(response);
+	}
+
+	/**
+	 * Загрузка ATR
+	 * 
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> executeAtrLoad(String name, Date startDate, Date endDate, String[] securities,
+			String maType, Integer taPeriod, String period, String calendar) {
+		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		List<NameValuePair> nvps = new ArrayList<>();
+		nvps.add(new BasicNameValuePair("startDate", sdf.format(startDate)));
+		nvps.add(new BasicNameValuePair("endDate", sdf.format(endDate)));
+		nvps.addAll(createList("securities", securities));
+		nvps.add(new BasicNameValuePair("maType", maType));
+		nvps.add(new BasicNameValuePair("taPeriod", taPeriod.toString()));
+		nvps.add(new BasicNameValuePair("period", period));
+		nvps.add(new BasicNameValuePair("calendar", calendar));
+
+		String response = executeHttpRequest("/LoadAtrRequest", nvps, name);
+		return (List<Map<String, Object>>) deserialize(response);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, Map<String, String>> executeBdpOverrideLoad(String name, List<SecForJobRequest> securities) {
+		Set<String> currencies = new HashSet<>();
+		List<NameValuePair> nvps = new ArrayList<>();
+		for (SecForJobRequest security : securities) {
+			currencies.add(security.iso);
+			String cursec = security.iso + security.code;
+			nvps.add(new BasicNameValuePair("securities", cursec));
+		}
+		nvps.addAll(createList("currencies", currencies.toArray(new String[currencies.size()])));
+
+		String response = executeHttpRequest("/LoadBdpOverrideRequest", nvps, name);
+		return (Map<String, Map<String, String>>) deserialize(response);
+	}
+
+	@Override
+	public void subscriptionStart(SubscriptionItem item, List<SecurityItem> securities) {
+		// Подписка из агента
+	}
+
+	@Override
+	public void subscriptionStop(SubscriptionItem item) {
+		// Подписка из агента
+	}
+}
--- jobber/src/main/java/ru/prbb/jobber/repo/BloombergServicesJ.java
+++ jobber/src/main/java/ru/prbb/jobber/repo/BloombergServicesJ.java
@@ -1,99 +1,14 @@
-/**
- * 
- */
 package ru.prbb.jobber.repo;
 
-import java.io.StringWriter;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.Set;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import ru.prbb.jobber.domain.AgentTask;
 import ru.prbb.jobber.domain.SecForJobRequest;
 import ru.prbb.jobber.domain.SecurityItem;
 import ru.prbb.jobber.domain.SubscriptionItem;
-import ru.prbb.jobber.services.AgentTaskService;
 
-/**
- * @author RBr
- */
-@Service
-public class BloombergServicesJ {
-
-	private Logger log = LoggerFactory.getLogger(getClass());
-
-	@Autowired
-	private AgentTaskService tasks;
-	private Random rnd = new Random(System.currentTimeMillis());
-
-	private String executeRequest(String name, Map<String, Object> data) throws InterruptedException {
-		long idTask = rnd.nextLong();
-		data.put("idTask", idTask);
-		data.put("name", name);
-		String json = serialize(data);
-		AgentTask task = new AgentTask(idTask, json);
-		tasks.add(task);
-		synchronized (task) {
-			for (int iter = 120, c = 0; c < iter; ++c) {
-				try {
-					task.wait(1000);
-				} catch (InterruptedException ignore) {
-				}
-
-				String result = task.getResult();
-
-				if (result == null) {
-					continue;
-				}
-
-				if ("WAIT".equals(result)) {
-					iter = 1200;
-					continue;
-				}
-
-				tasks.remove(task);
-				return result;
-			}
-		}
-		throw new InterruptedException("Agent task execute timeout");
-	}
-
-	private final ObjectMapper m = new ObjectMapper();
-
-	private String serialize(Object object) {
-		try {
-			StringWriter w = new StringWriter();
-			m.writeValue(w, object);
-			return w.toString();
-		} catch (Exception e) {
-			log.error("serialize", e);
-		}
-		throw new RuntimeException("serialize");
-	}
-
-	private Object deserialize(String content) {
-		try {
-			log.info(content);
-			return m.readValue(content, Object.class);
-		} catch (Exception e) {
-			log.error("deserialize", e);
-		}
-		throw new RuntimeException("deserialize");
-	}
+public interface BloombergServicesJ {
 
 	/**
 	 * BDS запрос
@@ -104,21 +19,7 @@ public class BloombergServicesJ {
 	 * @return [Peers, EARN_ANN_DT_TIME_HIST_WITH_EPS, ERN_ANN_DT_AND_PER,
 	 *         PeerTicker, BEST_ANALYST_RECS_BULK]
 	 */
-	@SuppressWarnings("unchecked")
-	public Map<String, Object> executeBdsRequest(String name, String[] securities, String[] fields) {
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "executeBdsRequest");
-		m.put("securities", securities);
-		m.put("fields", fields);
-
-		try {
-			String response = executeRequest(name, m);
-			return (Map<String, Object>) deserialize(response);
-		} catch (Exception e) {
-			log.error("BdsRequest", e);
-			throw new RuntimeException(e);
-		}
-	}
+	Map<String, Object> executeBdsRequest(String name, String[] securities, String[] fields);
 
 	/**
 	 * Выполнить запрос //blp/refdata, ReferenceDataRequest<br>
@@ -133,22 +34,8 @@ public class BloombergServicesJ {
 	 * @return security -> {field, value}
 	 * @throws Exception
 	 */
-	@SuppressWarnings("unchecked")
-	public Map<String, Map<String, String>> executeReferenceDataRequest(String name,
-			String[] securities, String[] fields) {
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "executeReferenceDataRequest");
-		m.put("securities", securities);
-		m.put("fields", fields);
-
-		try {
-			String response = executeRequest(name, m);
-			return (Map<String, Map<String, String>>) deserialize(response);
-		} catch (Exception e) {
-			log.error("ReferenceData", e);
-			throw new RuntimeException(e);
-		}
-	}
+	Map<String, Map<String, String>> executeReferenceDataRequest(String name,
+			String[] securities, String[] fields);
 
 	/**
 	 * Загрузка котировок
@@ -164,27 +51,8 @@ public class BloombergServicesJ {
 	 * @return security -> {date -> { field, value } }
 	 * @throws Exception
 	 */
-	@SuppressWarnings("unchecked")
-	public Map<String, Map<String, Map<String, String>>> executeHistoricalDataRequest(String name,
-			Date startDate, Date endDate, String[] securities, String[] fields) {
-		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "executeHistoricalDataRequest");
-		m.put("dateStart", sdf.format(startDate));
-		m.put("dateEnd", sdf.format(endDate));
-		m.put("securities", securities);
-		m.put("fields", fields);
-		m.put("currencies", null);
-
-		try {
-			String response = executeRequest(name, m);
-			return (Map<String, Map<String, Map<String, String>>>) deserialize(response);
-		} catch (Exception e) {
-			log.error("HistoricalDataRequest", e);
-			throw new RuntimeException(e);
-		}
-	}
+	Map<String, Map<String, Map<String, String>>> executeHistoricalDataRequest(String name,
+			Date startDate, Date endDate, String[] securities, String[] fields);
 
 	/**
 	 * Загрузка котировок
@@ -200,146 +68,21 @@ public class BloombergServicesJ {
 	 * @return security -> {date -> { field, value } }
 	 * @throws Exception
 	 */
-	@SuppressWarnings("unchecked")
-	public Map<String, Map<String, Map<String, String>>> executeHistoricalDataRequest(String name,
-			Date startDate, Date endDate, String[] securities, String[] fields, String[] currencies) {
-		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "executeHistoricalDataRequest");
-		m.put("dateStart", sdf.format(startDate));
-		m.put("dateEnd", sdf.format(endDate));
-		m.put("securities", securities);
-		m.put("fields", fields);
-		m.put("currencies", currencies);
-
-		try {
-			String response = executeRequest(name, m);
-			return (Map<String, Map<String, Map<String, String>>>) deserialize(response);
-		} catch (Exception e) {
-			log.error("HistoricalDataRequest", e);
-			throw new RuntimeException(e);
-		}
-	}
+	Map<String, Map<String, Map<String, String>>> executeHistoricalDataRequest(String name,
+			Date startDate, Date endDate, String[] securities, String[] fields, String[] currencies);
 
 	/**
 	 * Загрузка ATR
 	 * 
 	 * @throws Exception
 	 */
-	@SuppressWarnings("unchecked")
-	public List<Map<String, Object>> executeAtrLoad(String name, Date startDate, Date endDate, String[] securities,
-			String maType, Integer taPeriod, String period, String calendar) {
-		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+	List<Map<String, Object>> executeAtrLoad(String name, Date startDate, Date endDate, String[] securities,
+			String maType, Integer taPeriod, String period, String calendar);
 
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "executeAtrLoad");
-		m.put("dateStart", sdf.format(startDate));
-		m.put("dateEnd", sdf.format(endDate));
-		m.put("securities", securities);
-		m.put("maType", maType);
-		m.put("taPeriod", taPeriod);
-		m.put("period", period);
-		m.put("calendar", calendar);
+	Map<String, Map<String, String>> executeBdpOverrideLoad(String name, List<SecForJobRequest> securities);
 
-		try {
-			String response = executeRequest(name, m);
-			return (List<Map<String, Object>>) deserialize(response);
-		} catch (Exception e) {
-			log.error("LoadAtrRequest", e);
-			throw new RuntimeException(e);
-		}
-	}
+	void subscriptionStart(SubscriptionItem item, List<SecurityItem> securities);
 
-	@SuppressWarnings("unchecked")
-	public Map<String, Map<String, String>> executeBdpOverrideLoad(String name, List<SecForJobRequest> securities) {
-		Set<String> currencies = new HashSet<>();
-		List<String> cursecs = new ArrayList<>(securities.size());
-		for (SecForJobRequest security : securities) {
-			currencies.add(security.iso);
-			String cursec = security.iso + security.code;
-			cursecs.add(cursec);
-		}
-
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "executeBdpOverrideLoad");
-		m.put("cursecs", cursecs);
-		m.put("currencies", currencies);
-
-		try {
-			String response = executeRequest(name, m);
-			return (Map<String, Map<String, String>>) deserialize(response);
-		} catch (Exception e) {
-			log.error("LoadBdpOverrideRequest", e);
-			throw new RuntimeException(e);
-		}
-	}
-
-	// FIXME uriCallback
-	//private String URI_CALLBACK = "http://172.23.153.164:8080/Jobber/Subscription";
-
-	// Облако
-	//private String URI_CALLBACK = "http://192.168.100.101:8080/Jobber/Subscription";
-
-	// Облако редирект
-	private String URI_CALLBACK = "http://172.16.15.36:10180/Jobber/Subscription";
-
-	// Облако редирект
-	//private String URI_CALLBACK = "http://172.16.15.36:10190/Jobber/Subscription";
-
-	public void subscriptionStart(SubscriptionItem item, List<SecurityItem> securities) {
-		if (securities == null || securities.isEmpty())
-			return;
-
-		List<String> secs = new ArrayList<>(securities.size());
-		for (SecurityItem security : securities) {
-			secs.add(security.getCode());
-		}
-
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "SubscriptionStart");
-		m.put("id", item.getId());
-		m.put("uriCallback", URI_CALLBACK);
-		
-		m.put("securities", secs);
-
-		try {
-			String response = executeRequest("Start subscriptions" + item.getName(), m);
-			@SuppressWarnings("unchecked")
-			Map<String, Object> map = (Map<String, Object>) deserialize(response);
-			if (map.get("result").toString().contains("STARTED")) {
-				item.setSubscriptionId(map.get("subscriptionId"));
-				log.info(response);
-			} else {
-				log.error(response);
-			}
-		} catch (Exception e) {
-			log.error("SubscriptionStart", e);
-			throw new RuntimeException(e);
-		}
-	}
-
-	public void subscriptionStop(SubscriptionItem item) {
-		Map<String, Object> m = new HashMap<>();
-		m.put("type", "SubscriptionStop");
-		m.put("id", item.getId());
-		m.put("subscriptionId", item.getSubscriptionId());
-		m.put("uriCallback", URI_CALLBACK);
-
-		List<NameValuePair> nvps = new ArrayList<>();
-		nvps.add(new BasicNameValuePair("id", item.getId().toString()));
-
-		try {
-			String response = executeRequest("Stop subscriptions " + item.getName(), m);
-			if (response.contains("STOPPED")) {
-				log.info(response);
-			} else {
-				log.error(response);
-			}
-		} catch (Exception e) {
-			log.error("SubscriptionStop", e);
-			throw new RuntimeException(e);
-		}
-	}
+	void subscriptionStop(SubscriptionItem item);
 
 }
